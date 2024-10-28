c3e25419ff69c298e8913b8558b4def4275e4d2a
trentwirth <62706609+trentwirth@users.noreply.github.com>
2024-10-28 15:22:55 -0400

    tooling

--- reactiontimeapp/ui.py
+++ reactiontimeapp/ui.py
@@ -1,9 +1,26 @@
-
 from textual.widget import Widget
-from textual.widgets import Button 
+from textual.widgets import Button
+from textual import events
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 class ReactionTimeUI(Widget):
 
     def compose(self):
         yield Button("Start", id="start_button")
-    
+        yield Button("z", id="z_button")
+        yield Button("m", id="m_button")
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "start_button":
+            logging.info("Start button pressed")
+
+    async def on_key(self, event: events.Key) -> None:
+        if event.key == "z":
+            self.query_one("#z_button").press()
+            logging.info("z pressed")
+        elif event.key == "m":
+            self.query_one("#m_button").press()
+            logging.info("m pressed")
